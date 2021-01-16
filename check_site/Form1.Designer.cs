a1f1c7194314a452412427d7b0660808a8d36f4b
Alexey <lapadaviloff@yandex.ru>
2021-01-16 22:16:07 +0300

    baloon

--- check_site/Form1.Designer.cs
+++ check_site/Form1.Designer.cs
@@ -29,7 +29,39 @@ namespace check_site
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.списокСайтовToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.оПрограммеToolStripMenuItem,
+            this.списокСайтовToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(154, 48);
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.оПрограммеToolStripMenuItem.Text = "о программе";
+            // 
+            // списокСайтовToolStripMenuItem
+            // 
+            this.списокСайтовToolStripMenuItem.Name = "списокСайтовToolStripMenuItem";
+            this.списокСайтовToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.списокСайтовToolStripMenuItem.Text = "список сайтов";
             // 
             // Form1
             // 
@@ -38,12 +70,17 @@ namespace check_site
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem списокСайтовToolStripMenuItem;
     }
 }
 
--- check_site/Form1.cs
+++ check_site/Form1.cs
@@ -12,11 +12,78 @@ namespace check_site
 {
     public partial class Form1 : Form
     {
+        private FormWindowState _OldFormState;
+        private Icon empetyIcon;
+        private Icon checkMailIcon;
+
+
         public Form1()
         {
             InitializeComponent();
+            InitializeComponent();
+            //программа в правом нижнем углу
+            this.StartPosition = FormStartPosition.Manual;
+            var wArea = Screen.PrimaryScreen.WorkingArea;
+            this.Left = wArea.Width + wArea.Left - this.Width;
+            this.Top = wArea.Height + wArea.Top - this.Height;
+
+            //инициализация всплывающего сообщения
+
+            empetyIcon = new Icon("icon/empety.ico");
+            checkMailIcon = new Icon("icon/checkMail.ico");
+            notifyIcon1.Icon = SystemIcons.Exclamation;
+            notifyIcon1.BalloonTipTitle = "Balloon Tip Title";
+            notifyIcon1.BalloonTipText = "Balloon Tip Text.";
+            notifyIcon1.Visible = true;
+            notifyIcon1.ShowBalloonTip(30000);
+
+            //задаем всплывающий текст-подсказку (появляется при наведении указателя на иконку в трее)
+            notifyIcon1.Text = "Текст-подсказка";
+            //устанавливаем значок, отображаемый в трее:
+            //либо один из стандартных:
+            //notifyIcon1.Icon = SystemIcons.Error;
+            //либо свой из файла:
+            notifyIcon1.Icon = checkMailIcon;
+            //подписываемся на событие клика мышкой по значку в трее
+            notifyIcon1.MouseClick += new MouseEventHandler(_notifyIcon_MouseClick);
+            //подписываемся на событие изменения размера формы
+            this.Resize += new EventHandler(FormForTray_Resize);
+        }
+        void _notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            //проверяем, какой кнопкой было произведено нажатие
+            if (e.Button == MouseButtons.Left)//если левой кнопкой мыши
+            {
+                //проверяем текущее состояние окна
+                if (WindowState == FormWindowState.Normal || WindowState == FormWindowState.Maximized)//если оно развернуто
+                {
+                    //сохраняем текущее состояние
+                    _OldFormState = WindowState;
+                    //сворачиваем окно
+                    WindowState = FormWindowState.Minimized;
+                    //скрываться в трей оно будет по событию Resize (изменение размера), которое сгенерировалось после минимизации строчкой выше
+                }
+                else//в противном случае
+                {
+                    //и показываем на нанели задач
+                    Show();
+                    //разворачиваем (возвращаем старое состояние "до сворачивания")
+                    WindowState = _OldFormState;
+                }
+            }
         }
 
+        ///
+        /// обрабатываем событие изменения размера
+        ///
+        void FormForTray_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == WindowState)//если окно "свернуто"
+            {
+                //то скрываем его
+                Hide();
+            }
+        }
 
     }
 }
